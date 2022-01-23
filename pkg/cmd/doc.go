109e61c1bc82bbd6e58db9eef828ccc8d0b47169
Nicola Gallo <nicola.gallo@nitroagility.com>
2022-01-23 11:12:50 +0100

    Code refactoring

--- pkg/cmd/doc.go
+++ pkg/cmd/doc.go
@@ -13,10 +13,4 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-package main
-
-import "github.com/nitroci/nitroci-plugin-bitbucket/pkg/cmd"
-
-func main() {
-	cmd.Execute()
-}
+package cmd
